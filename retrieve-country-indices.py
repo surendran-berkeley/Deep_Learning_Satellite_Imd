3f452dfaeae6b41017c26129683f87933b915849
Brian Goodness <briangoodness@gmail.com>
2017-03-19 00:40:08 +0000

    Accepts command-line args for retrieve-country-indices.py

--- retrieve-country-indices.py
+++ retrieve-country-indices.py
@@ -1,14 +1,17 @@
-import wget
+#import wget
 import numpy as np
 import pandas as pd
 import time
 import os
 import os.path
+import sys
+import getopt
 from osgeo import gdal, ogr, osr
 from scipy import misc, ndimage
 import cStringIO
 gdal.UseExceptions()
 import urllib
+from retrying import retry
 
 # Helper function to read a raster file
 def read_raster(raster_file):
@@ -127,115 +130,141 @@ def get_shp_extent(shp_file):
     # x_min_shp, x_max_shp, y_min_shp, y_max_shp = extent
     return extent
 
-    # Helper functions to download images from Google Maps API
+# Helper functions to download images from Google Maps API
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=3600000)
+def save_img(url, file_path, file_name):
+    """
+    Function
+    --------
+    save_img
 
-    from retrying import retry
+    Given a url of the map, save the image
 
-    @retry(wait_exponential_multiplier=1000, wait_exponential_max=3600000)
-    def save_img(url, file_path, file_name):
-        """
-        Function
-        --------
-        save_img
+    Parameters
+    ----------
+    url : string
+        URL of the map from Google Map Static API
+    file_path : string
+        Folder name of the map
+    file_name : string
+        File name
 
-        Given a url of the map, save the image
+    Returns
+    -------
+    None
+    """
+    a = urllib.urlopen(url).read()
+    b = cStringIO.StringIO(a)
+    image = ndimage.imread(b, mode='RGB')
+    # when no image exists, api will return an image with the same color.
+    # and in the center of the image, it said'Sorry. We have no imagery here'.
+    # we should drop these images if large area of the image has the same color.
+    if np.array_equal(image[:,:10,:],image[:,10:20,:]):
+        pass
+    else:
+        misc.imsave(file_path + file_name, image[50:450, :, :])
 
-        Parameters
-        ----------
-        url : string
-            URL of the map from Google Map Static API
-        file_path : string
-            Folder name of the map
-        file_name : string
-            File name
+# print-out help / instructions
+def help():
+    print('\nusage:')
+    print('use --keyid= for setting the Google Maps Static API key number.')
+    print('use --topidx= for setting the top row to start looping over the country.')
+    print('(note: the top-left index should not be altered.)\n')
 
-        Returns
-        -------
-        None
-        """
-        a = urllib.urlopen(url).read()
-        b = cStringIO.StringIO(a)
-        image = ndimage.imread(b, mode='RGB')
-        # when no image exists, api will return an image with the same color.
-        # and in the center of the image, it said'Sorry. We have no imagery here'.
-        # we should drop these images if large area of the image has the same color.
-        if np.array_equal(image[:,:10,:],image[:,10:20,:]):
-            pass
-        else:
-            misc.imsave(file_path + file_name, image[50:450, :, :])
+# run main
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h", ["topidx=", "keyid="])
+    except getopt.GetoptError:
+        sys.exit(2)
 
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            help()
+            sys.exit(2)
+        elif opt == '--topidx':
+            top_idx = arg
 
-# retrieve nightlights data (run only once)
-#night_image_url = 'https://ngdc.noaa.gov/eog/data/web_data/v4composites/F182010.v4.tar'
-#wget.download(night_image_url)
+        elif opt == '--keyid':
+            key_id = arg
 
-# this illustrates how you can read the nightlight image
-raster_file = 'data/nightlights/F182010.v4/F182010.v4d_web.stable_lights.avg_vis.tif'
-x_size, top_left_x_coords, top_left_y_coords, centroid_x_coords, centroid_y_coords, bands_data = read_raster(raster_file)
+    # print top row index
+    print('top index row: %s' % top_idx)
 
-# save the result in compressed format - see https://docs.scipy.org/doc/numpy/reference/generated/numpy.savez.html
-#np.savez('data/nightlights/nightlight.npz', top_left_x_coords=top_left_x_coords, top_left_y_coords=top_left_y_coords, bands_data=bands_data)
+    # print key
+    key = os.getenv('GMAP_API_%s' % key_id)
+    print('key variable: GMAP_API_%s, key: %s' % (key_id, key))
 
-# Now read in the shapefile for Rwanda and extract the edges of the country
-countries = ['malawi', 'nigeria', 'rwanda', 'tanzania', 'uganda']
-#countries = ['rwanda']
+    # retrieve nightlights data (run only once)
+    #night_image_url = 'https://ngdc.noaa.gov/eog/data/web_data/v4composites/F182010.v4.tar'
+    #wget.download(night_image_url)
 
-df = pd.DataFrame()
+    # this illustrates how you can read the nightlight image
+    raster_file = 'data/nightlights/F182010.v4/F182010.v4d_web.stable_lights.avg_vis.tif'
+    x_size, top_left_x_coords, top_left_y_coords, centroid_x_coords, centroid_y_coords, bands_data = read_raster(raster_file)
 
-country_shp_files = {
-    'malawi':'MWGE71FL.shp',
-    'nigeria':'NGGE71FL.shp',
-    'rwanda':'Sector_Boundary_2012.shp',
-    'tanzania':'TZGE7AFL.shp',
-    'uganda':'UGGE71FL.shp',
-}
+    # save the result in compressed format - see https://docs.scipy.org/doc/numpy/reference/generated/numpy.savez.html
+    #np.savez('data/nightlights/nightlight.npz', top_left_x_coords=top_left_x_coords, top_left_y_coords=top_left_y_coords, bands_data=bands_data)
 
-for country in countries:
-    #inShapefile = 'data/shapefiles/rwanda/Sector_Boundary_2012.shp'
-    inShapefile = "data/shapefiles/%s/%s" % ( country, country_shp_files[country] )
-    x_min_shp, x_max_shp, y_min_shp, y_max_shp = get_shp_extent(inShapefile)
+    # Now read in the shapefile for Rwanda and extract the edges of the country
+    #countries = ['malawi', 'nigeria', 'rwanda', 'tanzania', 'uganda']
+    countries = ['malawi']
 
-    left_idx, top_idx = get_cell_idx(x_min_shp, y_max_shp, top_left_x_coords, top_left_y_coords )
-    right_idx, bottom_idx = get_cell_idx(x_max_shp, y_min_shp, top_left_x_coords, top_left_y_coords)
+    df = pd.DataFrame()
 
-    num_images = (bottom_idx - top_idx) * (right_idx - left_idx)
-    row = {
-      'country':country,
-      'left_idx':left_idx,
-      'top_idx':top_idx,
-      'right_idx':right_idx,
-      'bottom_idx':bottom_idx,
-      'num_images':num_images,
+    country_shp_files = {
+        'malawi':'MWGE71FL.shp',
+        'nigeria':'NGGE71FL.shp',
+        'rwanda':'Sector_Boundary_2012.shp',
+        'tanzania':'TZGE7AFL.shp',
+        'uganda':'UGGE71FL.shp',
     }
-    df = df.append(row, ignore_index=True)
 
-columns = ['country', 'left_idx', 'top_idx', 'right_idx', 'bottom_idx', 'num_images']
-df = df[columns]
-print df
-df.to_csv('country-indices-num-images.csv')
+    for country in countries:
+        inShapefile = "data/shapefiles/%s/%s" % ( country, country_shp_files[country] )
+        x_min_shp, x_max_shp, y_min_shp, y_max_shp = get_shp_extent(inShapefile)
 
-# Download Daytime Satellite Imagery; retrieve and save images
-'''
-key = os.getenv('GMAP_API_KEY')
-m = 1
-for i in xrange(left_idx, right_idx + 1):
-    for j in xrange(top_idx, bottom_idx + 1):
-        lon = centroid_x_coords[i]
-        lat = centroid_y_coords[j]
-        url = 'https://maps.googleapis.com/maps/api/staticmap?center=' + str(lat) + ',' + \
-               str(lon) + '&zoom=16&size=400x500&maptype=satellite&key=' + key
-        lightness = bands_data[j, i, 0]
-        #file_path = 'google_image/' + str(lightness) + '/'
-        file_path = 'data/google_image/' + str(lightness) + '/'
-        if not os.path.isdir(file_path):
-            os.makedirs(file_path)
-        file_name = str(i) + '_' + str(j) +'.png'
-        print 'm: %s, lightness: %s, file_name: %s, url: %s' % (m, lightness, file_name, url)
-        #save_img(url, file_path, file_name)
-        if m % 100 == 0:
-            print m
-        m += 1
-        if m == 24000:
-            print 'i: %s, j:%s' % (i, j)
-            stop
-'''
+        left_idx, top_idx = get_cell_idx(x_min_shp, y_max_shp, top_left_x_coords, top_left_y_coords )
+        right_idx, bottom_idx = get_cell_idx(x_max_shp, y_min_shp, top_left_x_coords, top_left_y_coords)
+
+        num_images = (bottom_idx - top_idx) * (right_idx - left_idx)
+        row = {
+          'country':country,
+          'left_idx':left_idx,
+          'top_idx':top_idx,
+          'right_idx':right_idx,
+          'bottom_idx':bottom_idx,
+          'num_images':num_images,
+        }
+        df = df.append(row, ignore_index=True)
+
+        # Download Daytime Satellite Imagery; retrieve and save images
+        m = 1
+        for i in xrange(left_idx, right_idx + 1):
+            for j in xrange(top_idx, bottom_idx + 1):
+                lon = centroid_x_coords[i]
+                lat = centroid_y_coords[j]
+                url = 'https://maps.googleapis.com/maps/api/staticmap?center=' + str(lat) + ',' + \
+                       str(lon) + '&zoom=16&size=400x500&maptype=satellite&key=' + key
+                lightness = bands_data[j, i, 0]
+                file_path = '/data/google_image/%s/' % country + str(lightness) + '/'
+                if not os.path.isdir(file_path):
+                    os.makedirs(file_path)
+                file_name = str(i) + '_' + str(j) +'.png'
+                print 'm: %s, lightness: %s, file_name: %s, url: %s' % (m, lightness, file_name, url)
+                save_img(url, file_path, file_name)
+                if m % 100 == 0:
+                    print m
+                m += 1
+                if m == 24000:
+                    print 'i: %s, j:%s' % (i, j)
+                    stop
+
+    # write indices for all countries' shape files
+    columns = ['country', 'left_idx', 'top_idx', 'right_idx', 'bottom_idx', 'num_images']
+    df = df[columns]
+    print df
+    #df.to_csv('country-indices-num-images.csv')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
