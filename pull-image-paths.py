adfdfc8befca428c33f609b18400e3fb45f5937a
Brian Goodness <briangoodness@gmail.com>
2017-03-21 23:00:04 +0000

    Updated pull-image-paths.py; removed *.csv files from repo tracking

--- pull-image-paths.py
+++ pull-image-paths.py
@@ -1,5 +1,14 @@
+import sys
+import getopt
 import glob
 import pandas as pd
+
+# print-out help / instructions
+def help():
+    print('\nusage:')
+    print('python pull-image-paths.py --path=/path/to/image/directory')
+    print('use --path= for setting the path to the directory with satellite images.')
+    print('(note: a generic file called "image-index-summary.csv" will be written to the current folder.)\n')
 
 # run main
 def main(argv):
@@ -15,8 +24,6 @@ def main(argv):
             sys.exit(2)
         elif opt == '--path':
             path = arg
-        elif opt == '--country':
-                    country = arg
 
     # retrieve file paths and luminosity
     files = glob.glob('%s/*/*_*.png' % path)
@@ -26,10 +33,8 @@ def main(argv):
     indices = pd.DataFrame(df.filename.str.replace('.png','').str.split(pat='_', expand=True)[[0,1]]).astype(int)
     indices.rename(columns={0:'i', 1:'j'}, inplace=True)
     indices['ct'] = 1
-    summary = indices.set_index(['i','j']).unstack('i').stack().reset_index().groupby('j').sum().sort('ct')
-    summary.to_csv('%s/image-index-summary-%s.csv' % (path, country))
+    summary = indices.set_index(['i','j']).unstack('i').stack().reset_index().groupby('j').sum().reset_index().sort(['ct','j','i'], ascending=[True, False, True])
+    summary.to_csv('image-index-summary.csv')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
